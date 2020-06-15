3a5107dca5efe254423e292e7a5c4a54a544af62
Duvvvy <ryanbircham@gmail.com>
2020-06-15 15:40:44 +1200

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders SEER', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/SEER/i);
+  const { getByText } = render(<MemoryRouter><App /></MemoryRouter>);
+  const linkElement = getByText(/Evidence-based software engineering/i);
   expect(linkElement).toBeInTheDocument();
 });
